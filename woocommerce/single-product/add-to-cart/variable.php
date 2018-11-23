2384f53baf2f26f7435732145d798139378366a1
Ville Könönen <ville@nomasi.com>
2018-11-23 19:18:32 +0400

    Checked overriding variable.php template against WC v3.4.1

--- woocommerce/single-product/add-to-cart/variable.php
+++ woocommerce/single-product/add-to-cart/variable.php
@@ -7,6 +7,8 @@
  * On occasion WooCommerce will need to update template files and you (the theme developer).
  * will need to copy the new files to your theme to maintain compatibility.
  *
+ * Last checked against WC version 3.4.1
+ *
  * @see     WC_SizeMe_Measurements::locate_template
  * @package SizeMe Measurements
  * @since   1.0.0
@@ -43,15 +45,11 @@ $attribute_keys = array_keys( $attributes );
 
 do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 
-<form class="variations_form cart" method="post" enctype='multipart/form-data'
-      data-product_id="<?php echo absint( $product->id ); ?>"
-      data-product_variations="<?php echo htmlspecialchars( wp_json_encode( $available_variations ) ) ?>">
+<form class="variations_form cart" action="<?php echo esc_url( apply_filters( 'woocommerce_add_to_cart_form_action', $product->get_permalink() ) ); ?>" method="post" enctype='multipart/form-data' data-product_id="<?php echo absint( $product->get_id() ); ?>" data-product_variations="<?php echo htmlspecialchars( wp_json_encode( $available_variations ) ); // WPCS: XSS ok. ?>">
 	<?php do_action( 'woocommerce_before_variations_form' ); ?>
 
 	<?php if ( empty( $available_variations ) && false !== $available_variations ) : ?>
-		<p class="stock out-of-stock">
-			<?php esc_attr_e( 'This product is currently out of stock and unavailable.', 'woocommerce' ); ?>
-		</p>
+		<p class="stock out-of-stock"><?php esc_attr_e( 'This product is currently out of stock and unavailable.', 'woocommerce' ); ?></p>
 	<?php else : ?>
 		<table class="variations" cellspacing="0">
 			<tbody>
@@ -74,29 +72,21 @@ do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 					$class = '';
 				} ?>
 				<tr>
-					<td class="label">
-						<label for="<?php echo sanitize_title( $attribute_name ); ?>">
-							<?php echo wc_attribute_label( $attribute_name ); ?>
-						</label>
-					</td>
+					<td class="label"><label for="<?php echo esc_attr( sanitize_title( $attribute_name ) ); ?>"><?php echo wc_attribute_label( $attribute_name ); // WPCS: XSS ok. ?></label></td>
 					<td class="value<?php echo $class !== '' ? ' ' . esc_attr( $class ) : ''; ?>">
 						<?php
-						wc_dropdown_variation_attribute_options( array(
-							'options'   => $options,
-							'attribute' => $attribute_name,
-							'product'   => $product,
-							'selected'  => $selected,
-						) );
-						echo end( $attribute_keys ) === $attribute_name ? apply_filters( 'woocommerce_reset_variations_link',
-						'<a class="reset_variations" href="#">' . __( 'Clear', 'woocommerce' ) . '</a>' ) : '';
+							wc_dropdown_variation_attribute_options( array(
+								'options'   => $options,
+								'attribute' => $attribute_name,
+								'product'   => $product,
+							) );
+							echo end( $attribute_keys ) === $attribute_name ? wp_kses_post( apply_filters( 'woocommerce_reset_variations_link', '<a class="reset_variations" href="#">' . esc_html__( 'Clear', 'woocommerce' ) . '</a>' ) ) : '';
 						?>
 					</td>
 				</tr>
 			<?php endforeach; ?>
 			</tbody>
 		</table>
-
-		<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
 
 		<div class="single_variation_wrap">
 			<?php
@@ -123,7 +113,6 @@ do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 			?>
 		</div>
 
-		<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
 	<?php endif; ?>
 
 	<?php do_action( 'woocommerce_after_variations_form' ); ?>
